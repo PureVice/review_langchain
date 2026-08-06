1cec0aaa942c1b6c59a0668d11074a39b9eda3bb
PureVice <vhugosantos2@gmail.com>
2026-08-06 11:45:47 -0300

    test

--- tests/test_app.py
+++ tests/test_app.py
@@ -75,3 +75,6 @@ def test_analyze_post_empty_text(mock_save, mock_analyze, client):
     mock_analyze.assert_not_called()
     mock_save.assert_not_called()
     assert response.status_code == 302
+    
+def test_ci():
+    assert 1==2
